883876acc23a366020b952a7d6d8c6de5bbc4811
nishimura.ryo <ryouroad@yahoo.co.jp>
2024-03-19 21:53:26 +0900

    rename

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/web/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/boardgame_bot_challenge/' : '/'
 })
